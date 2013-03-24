b9508ccbf51d21d1834cff8e49751b163bc5b80a
Daryl Robbins <daryl@robbins.name>
2013-03-24 09:42:22 -0400

    Updates from chapter 5

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
-describe "User Pages" do
+describe "User pages" do
 
-	subject { page }
+  subject { page }
 
-	describe "signup page" do
-		
-		before { visit signup_path }
+  describe "signup page" do
+    before { visit signup_path }
 
-		it { should have_selector('h1', text: 'Signup') }
-		
-	end
-
+    it { should have_selector('h1',    text: 'Signup') }
+  end
 end
